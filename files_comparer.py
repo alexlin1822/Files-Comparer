5d802410f416a173ae2e7860c580f20b1f862dd7
Alex <alexlin1822@hotmail.com>
2024-01-29 19:29:51 -0500

    code v1 done

--- files_comparer.py
+++ files_comparer.py
@@ -36,29 +36,11 @@ def get_md5_array(folder_path):
     return np_md5_array
 
 
-# def compare_files(file1, file2):
-#     # Open the files for reading
-
-#     folder1=get_md5_array(file1)
-#     folder2 = get_md5_array(file2)
-
-#     # with open(file1, 'rb') as f1, open(file2, 'rb') as f2:
-#     #     # Read the contents of the files
-#     #     content1 = f1.read()
-#     #     content2 = f2.read()
-
-#     #     if len(content1) != len(content2):
-#     #         return False
-
-#     #     # Compare the contents byte by byte
-#     #     for byte1, byte2 in zip(content1, content2):
-#     #         if byte1 != byte2:
-#     #             return False
-
-#     #     return True
-
-
 def compare_folders(folder1, folder2, replace_older_files):
+    """Compare two folders and print the results."""
+    """If replace_older_files is True, then delete the older files in older folder"""
+    """else, print the identical files in older folder"""
+
     identical_old_files = []
     # different_old_files = []
     print("Checking files in ", folder1)
@@ -66,109 +48,46 @@ def compare_folders(folder1, folder2, replace_older_files):
     print("Checking files in ", folder1)
     new_folder_array = get_md5_array(folder2)
 
+    print("Comparing files...")
     for old_file_array in old_folder_array:
         if np.any(new_folder_array[:, 0] == old_file_array[0]):
-            # print(f"File {old_file_array[1]}  is duplicated!")
             identical_old_files.append(old_file_array[1])
         # else:
-        #     # print(f"File {old_file_array[1]}  is unqiue!")
         #     different_old_files.append(old_file_array[1])
-    # return True
-
-    # Get the list of files in each folder
-    # files1 = os.listdir(folder1)
-    # files2 = os.listdir(folder2)
-
-    # # Sort the lists of files to ensure proper comparison
-    # files1.sort()
-    # files2.sort()
-    # i = 0
-
-    # for file1, file2 in zip(files1, files2):
-    #     file1_path = os.path.join(folder1, file1)
-    #     file2_path = os.path.join(folder2, file2)
-
-    #     file1_md5 = calculate_md5(file1_path)
-    #     file2_md5 = calculate_md5(file2_path)
-
-    #     if file1_md5 == file2_md5:
-    #         #         identical_old_files.append(file1_path)
-    #         print(f"Files {file1} and {file2} are identical.")
-    #     else:
-    #         #         different_old_files.append(file1_path)
-    #         print(f"Files {file1} and {file2} are different.")
-
-    # # Check if both files exist and are regular files
-    # if os.path.isfile(file1_path) and os.path.isfile(file2_path):
-    #     i += 1
-    #     if compare_files(file1_path, file2_path):
-    #         identical_old_files.append(file1_path)
-    #         # print(f"Files {file1} and {file2} are identical.")
-    #     else:
-    #         different_old_files.append(file1_path)
-    #         # print(f"Files {file1} and {file2} are different.")
-    #     if i % 100 == 0:
-    #         print("checked: ", i)
-    # else:
-    #     print(f"Skipping comparison for {file1} and {file2}.")
-
-    # # Iterate through files in both folders and compare
-    # for file1, file2 in zip(files1, files2):
-    #     file1_path = os.path.join(folder1, file1)
-    #     file2_path = os.path.join(folder2, file2)
-    #     # Check if both files exist and are regular files
-    #     if os.path.isfile(file1_path) and os.path.isfile(file2_path):
-    #         i += 1
-    #         if compare_files(file1_path, file2_path):
-    #             identical_old_files.append(file1_path)
-    #             # print(f"Files {file1} and {file2} are identical.")
-    #         else:
-    #             different_old_files.append(file1_path)
-    #             # print(f"Files {file1} and {file2} are different.")
-    #         if i % 100 == 0:
-    #             print("checked: ", i)
-    #     else:
-    #         print(f"Skipping comparison for {file1} and {file2}.")
-
-    # for different_old_file in different_old_files:
-    #     file2_path = os.path.join(folder2, file2)
-
-    #     # Check if both files exist and are regular files
-    #     if os.path.isfile(different_old_file) and os.path.isfile(file2_path):
-    #         if compare_files(file1_path, file2_path):
-    #             identical_old_files.append(file1_path)
-    #             # print(f"Files {file1} and {file2} are identical.")
-    #         else:
-    #             different_old_files.append(file1_path)
-    #             # print(f"Files {file1} and {file2} are different.")
-    #     else:
-    #         print(f"Skipping comparison for {file1} and {file2}.")
 
     # Print the results
     if replace_older_files:
+        print("Deleteing files...")
+        i = 0
+        j = 0
         # Replace the older files with the newer ones
         for file_path in identical_old_files:
             if os.path.exists(file_path):
                 os.remove(file_path)
                 print(f"File '{file_path}' has been deleted.")
+                i += 1
             else:
                 print(f"File '{file_path}' does not exist.")
+                j += 1
+        print(f'Deleted {i} files! {j} files do not exist!')
 
     else:
-        print(f"Identical files: {identical_old_files}")
+
+        print(f"=============================================")
+        print(f"Identical files:")
+        for file_path in identical_old_files:
+            print(f"  {file_path}")
+        print(f"=============================================")
 
 
 if __name__ == '__main__':
-
     # Example usage:
-    old_folder_path = 'D:\\PhonePhoto\\2023-09-03'
-    new_folder_path = 'D:\\PhonePhoto\\2023-11-24\\DCIM\\Camera'
+    old_folder_path = 'G:\\DELL\\D\\PhonePhoto\\2023.02.04\\Camera'
+    new_folder_path = 'G:\\DELL\\D\\PhonePhoto\\2023-11-24\\DCIM\\Camera'
 
     # old_folder_path = 'D:\\PhonePhoto\\a'
     # new_folder_path = 'D:\\PhonePhoto\\b'
     replace_older_files = True
+    # replace_older_files = False
 
     compare_folders(old_folder_path, new_folder_path, replace_older_files)
-
-    # compare_files('D:\\PhonePhoto\\2023-09-03\\IMG_20230521_131653.jpg',
-    #   'D:\\PhonePhoto\\2023-09-03\\IMG_20230521_131655.jpg')
